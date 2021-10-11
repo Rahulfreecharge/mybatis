d44c61e5bda2e34b3e2a1eb3c10bd97909e53f82
Rahulfreecharge <91590439+Rahulfreecharge@users.noreply.github.com>
2021-10-11 18:58:17 +0530

    Add files via upload

---
+++ mybatisnew/src/main/java/myBatisInsert.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+
+public class myBatisInsert { 
+
+   public static void main(String args[]) throws IOException{
+      
+      Reader reader = Resources.getResourceAsReader("SqlMapConfig.xml");
+      SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);		
+      SqlSession session = sqlSessionFactory.openSession();
+      
+      //Create a new student object
+      Student student = new Student("Arun","ECE", 70, 984767222, "aman89@gmail.com" ); 
+            
+      //Insert student data      
+      session.insert("Student.insert", student);
+      System.out.println("record inserted successfully");
+      session.commit();
+      session.close();
+	}
+}
